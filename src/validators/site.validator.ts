fcb9bb5550d2c6d109ea9228040a105fc699ef7b
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-04-01 18:42:01 +0530

    remove the required

--- src/validators/site.validator.ts
+++ src/validators/site.validator.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createSiteSchema = z.object({
   name: z.string().min(1, "Site name is required"),
   userName: z.string().min(1, "User name is required"),
-  url: z.string().url("Invalid URL").optional().or(z.literal("")),
+  url: z.string().min(1, "URL is required").optional().or(z.literal("")),
   password: z.string().min(1, "Password is required"),
 });
 
