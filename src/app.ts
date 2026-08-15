eccca20f2b19d36a1cd41796fb62dd91fa77a432
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-08-15 11:49:02 +0530

    Triger

--- src/app.ts
+++ src/app.ts
@@ -51,6 +51,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api/auth", authRoutes);
